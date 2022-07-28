30f43a78bf691b06cb99d2f4f25f59d4af02cc02
ESHamza <essaadihamza4@outlook.com>
2022-07-28 20:07:47 +0100

    created store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,36 @@
 import { createStore } from 'vuex'
-
 const store=createStore({
- state:{},
- mutations:{},
- getters:{},
- action:{}
+ state:{
+  IMAGES:[]
+ },
+ mutations:{
+  ADD_IMAGES(state,imgs_array){
+   state.IMAGES=imgs_array
+   console.log(state.IMAGES)
+  },
+  FLIP_UP_IMAGE(state,index){
+   state.IMAGES[index].flip=true
+   console.log(state.IMAGES)
+
+  }
+ },
+ getters:{
+  get_images(state){
+   return state.IMAGES
+  }
+ },
+ actions:{
+  async fetch_images({commit}){
+   try{
+    const Cards_object = await fetch('Data/data.json')
+    const Cards = await Cards_object.json()
+    commit('ADD_IMAGES',Cards.IMAGES)
+   }
+   catch(error){
+    console.log(error)
+  }
+ },
+}
 })
 
 export default store
